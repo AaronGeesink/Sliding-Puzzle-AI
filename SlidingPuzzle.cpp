3cfba8fe27e5978e6ac26057411e4775870101b2
roveredpwn <roverdog56@gmail.com>
2023-05-15 04:07:13 -0700

    Implemented uniform cost search with no heuristic

--- SlidingPuzzle.cpp
+++ SlidingPuzzle.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <ios> //used to get stream size
-#include <limits> //used to get numeric limits
+#include <ios>
+#include <limits>
+#include <unordered_map>
+#include <queue>
 using namespace std;
 
 // Function definitions
 int handleIntegerInput(int maxSelection);
-void createDefaultPuzzle(vector<vector<int>>& currentState, vector<vector<int>>& goalState);
-int createCustomPuzzle(vector<vector<int>>& currentState, vector<vector<int>>& goalState);
-bool puzzleIsValid(vector<vector<int>>& puzzle, int size);
-void printPuzzle(vector<vector<int>>& puzzle, int size);
+void createDefaultPuzzle(vector<vector<int>>& initialState, vector<vector<int>>& goalState);
+void createCustomPuzzle(vector<vector<int>>& initialState, vector<vector<int>>& goalState);
+bool puzzleIsValid(vector<vector<int>>& puzzle);
+void printPuzzle(vector<vector<int>>& puzzle);
+string puzzleToString(vector<vector<int>>& puzzle);
+
+void uniformCostSearch(vector<vector<int>>& initialState, vector<vector<int>>& goalState);
 
 
-struct BoardState {
-	vector<vector<int>> state;
-	int searchDepth;
+struct BoardStateNode {
+	vector<vector<int>> state; // state of this node
+	int depth; // depth that this node was expanded
 };
 
 int main() {
 
 	int input = 0; // Stores user input for menu selection
-	vector<vector<int>> currentState; // The state we will expand
+	vector<vector<int>> initialState; // The state we will expand
 	vector<vector<int>> goalState; // The goal state we wish to reach
-	int puzzleSize = 0; // NxN size of the puzzle
 
 	cout << "Welcome to the Sliding Puzzle Solver. Please select a puzzle type:\n";
 	cout << "1. Use a default puzzle\n";
@@ -32,18 +36,17 @@ int main() {
 	/* This section creates the initial state and goal state for the puzzle to be solved. */
 
 	if (input == 1) { // Create a default puzzle
-		createDefaultPuzzle(currentState, goalState);
-		puzzleSize = 3;
+		createDefaultPuzzle(initialState, goalState);
 	}
 	else if (input == 2) { // Create a custom puzzle
-		puzzleSize = createCustomPuzzle(currentState, goalState);
+		createCustomPuzzle(initialState, goalState);
 	}
 
 	cout << "\nPuzzle Info\n";
 	cout << "Initial State:\n";
-	printPuzzle(currentState, puzzleSize);
+	printPuzzle(initialState);
 	cout << "Goal State:\n";
-	printPuzzle(goalState, puzzleSize);
+	printPuzzle(goalState);
 
 	cout << "\nPlease select an algorithm to use:\n";
 	cout << "1. Uniform Cost Search\n";
@@ -52,12 +55,12 @@ int main() {
 	input = handleIntegerInput(3);
 
 	if (input == 1) { // Uniform Cost Search
-		
+		uniformCostSearch(initialState, goalState);
 	}
 	else if (input == 2) { // A* with Misplaced Tile Heuristic
 
 	}
-	else if (input == 3) { // A* with Manhatten Distance Heuristic
+	else if (input == 3) { // A* with Manhattan Distance Heuristic
 
 	}
 
@@ -67,10 +70,6 @@ int main() {
 /*
 A helper function to easily obtain and validate user input for integers.
 Allows for inputs from 1 to maxSelection
-Input:
-	maxSelection, the maximum input allowed
-Output:
-	int, the integer selection
 */
 int handleIntegerInput(int maxSelection) {
 	int input = 0;
@@ -94,7 +93,11 @@ int handleIntegerInput(int maxSelection) {
 	return input;
 }
 
-void createDefaultPuzzle(vector<vector<int>> &currentState, vector<vector<int>> &goalState) {
+/*
+Creates a default 3x3 puzzle.
+The user can select between 8 puzzles of increasing difficulty.
+*/
+void createDefaultPuzzle(vector<vector<int>> &initialState, vector<vector<int>> &goalState) {
 	int input = 0;
 	cout << "\nPlease select puzzle difficulty (higher depth is more difficult)\n";
 	cout << "1. Depth 0\n";
@@ -108,27 +111,160 @@ void createDefaultPuzzle(vector<vector<int>> &currentState, vector<vector<int>> 
 
 	input = handleIntegerInput(8);
 	switch(input) {
-		case 1: currentState = {{1,2,3}, {4,5,6}, {7,8,0}}; break; // depth 0
-		case 2: currentState = {{1,2,3}, {4,5,6}, {0,7,8}}; break; // depth 2
-		case 3:	currentState = {{1,2,3}, {5,0,6}, {4,7,8}}; break; // depth 4
-		case 4:	currentState = {{1,3,6}, {5,0,2}, {4,7,8}}; break; // depth 8
-		case 5:	currentState = {{1,3,6}, {5,0,7}, {4,8,2}}; break; // depth 12
-		case 6:	currentState = {{1,6,7}, {5,0,3}, {4,8,2}}; break; // depth 16
-		case 7:	currentState = {{7,1,2}, {4,8,5}, {6,3,0}}; break; // depth 20
-		case 8:	currentState = {{0,7,2}, {4,6,1}, {3,5,8}}; break; // depth 24
+		case 1: initialState = {{1,2,3}, {4,5,6}, {7,8,0}}; break; // depth 0
+		case 2: initialState = {{1,2,3}, {4,5,6}, {0,7,8}}; break; // depth 2
+		case 3:	initialState = {{1,2,3}, {5,0,6}, {4,7,8}}; break; // depth 4
+		case 4:	initialState = {{1,3,6}, {5,0,2}, {4,7,8}}; break; // depth 8
+		case 5:	initialState = {{1,3,6}, {5,0,7}, {4,8,2}}; break; // depth 12
+		case 6:	initialState = {{1,6,7}, {5,0,3}, {4,8,2}}; break; // depth 16
+		case 7:	initialState = {{7,1,2}, {4,8,5}, {6,3,0}}; break; // depth 20
+		case 8:	initialState = {{0,7,2}, {4,6,1}, {3,5,8}}; break; // depth 24
 		default: cout << "Fatal error. terminating program.\n"; exit(1); // should never reach this
 	}
 	goalState = {{1,2,3}, {4,5,6}, {7,8,0}};
 }
 
-int createCustomPuzzle(vector<vector<int>> &currentState, vector<vector<int>> &goalState) {
-	return 0;
+void createCustomPuzzle(vector<vector<int>> &initialState, vector<vector<int>> &goalState) {
+	
 }
 
-void printPuzzle(vector<vector<int>> &puzzle, int size) {
-	for(int i = 0; i < size; i++){
-		for(int j = 0; j < size; j++)
+void printPuzzle(vector<vector<int>> &puzzle) {
+	for(int i = 0; i < puzzle.size(); i++) {
+		for(int j = 0; j < puzzle[i].size(); j++)
 			cout << puzzle[i][j] << " ";
 		cout << endl;
 	}
+}
+
+string puzzleToString(vector<vector<int>> &puzzle)
+{
+	string puzzleString = "";
+	for(int i = 0; i < puzzle.size(); i++) {
+		for(int j = 0; j < puzzle[i].size(); j++)
+			puzzleString.append(to_string(puzzle[i][j]));
+	}
+	return puzzleString;
+}
+
+void uniformCostSearch(vector<vector<int>> &initialState, vector<vector<int>> &goalState) {
+	queue<BoardStateNode> nodesToExpand; // queue of nodes to expand
+	unordered_map<string, bool> repeatedStates; // an unordered_map used to keep track of repeated states
+	int numberNodesExpanded = 0;
+	int maxQueueSize = 1;
+
+	BoardStateNode initialNode;
+	initialNode.state = initialState;
+	initialNode.depth = 0;
+
+	nodesToExpand.push(initialNode);
+	string initialKey = puzzleToString(initialState);
+	repeatedStates.insert({initialKey, true});
+
+	// function ends either when goal is found, or when full search tree is exhausted
+	while(!nodesToExpand.empty()) {
+		BoardStateNode currentNode = nodesToExpand.front();
+		nodesToExpand.pop();
+
+		int currentDepth = currentNode.depth + 1;
+		numberNodesExpanded++;
+
+		// Goal test
+		if (currentNode.state == goalState) {
+			cout << "\nGoal state!\n\n";
+			cout << "Solution depth was " << currentNode.depth << endl;
+			cout << "Number of nodes expanded: " << numberNodesExpanded << endl;
+			cout << "Max queue size: " << maxQueueSize;
+			return;
+		}
+
+		// Find the the location of the empty tile (labeled zero)
+		int emptyRow = -1;
+		int emptyCol = -1;
+		for(int i = 0; i < currentNode.state.size(); i++) {
+			for(int j = 0; j < currentNode.state[i].size(); j++)
+				if (currentNode.state[i][j] == 0) {
+					emptyRow = i;
+					emptyCol = j;
+				}
+		}
+		
+		// Apply 4 operators: move left, move right, move up, move down.
+		// Operator directions are based on the movement of the empty tile.
+
+		// move left
+		if (emptyCol > 0) {
+			vector<vector<int>> consideredState = currentNode.state;
+			consideredState[emptyRow][emptyCol] = consideredState[emptyRow][emptyCol-1];
+			consideredState[emptyRow][emptyCol-1] = 0;
+
+			// Enqueue the node only if it has not been seen before
+			string newKey = puzzleToString(consideredState);
+			if (repeatedStates.find(newKey) == repeatedStates.end()) {
+				BoardStateNode newNode;
+				newNode.state = consideredState;
+				newNode.depth = currentDepth;
+				nodesToExpand.push(newNode);
+				repeatedStates.insert({newKey, true});
+			}	
+		}
+
+		// move right
+		if (emptyCol < initialState.size()-1) {
+			vector<vector<int>> consideredState = currentNode.state;
+			consideredState[emptyRow][emptyCol] = consideredState[emptyRow][emptyCol+1];
+			consideredState[emptyRow][emptyCol+1] = 0;
+
+			// Enqueue the node only if it has not been seen before
+			string newKey = puzzleToString(consideredState);
+			if (repeatedStates.find(newKey) == repeatedStates.end()) {
+				BoardStateNode newNode;
+				newNode.state = consideredState;
+				newNode.depth = currentDepth;
+				nodesToExpand.push(newNode);
+				repeatedStates.insert({newKey, true});
+			}	
+		}
+
+		// move up
+		if (emptyRow > 0) {
+			vector<vector<int>> consideredState = currentNode.state;
+			consideredState[emptyRow][emptyCol] = consideredState[emptyRow-1][emptyCol];
+			consideredState[emptyRow-1][emptyCol] = 0;
+
+			// Enqueue the node only if it has not been seen before
+			string newKey = puzzleToString(consideredState);
+			if (repeatedStates.find(newKey) == repeatedStates.end()) {
+				BoardStateNode newNode;
+				newNode.state = consideredState;
+				newNode.depth = currentDepth;
+				nodesToExpand.push(newNode);
+				repeatedStates.insert({newKey, true});
+			}	
+		}
+
+		// move down
+		if (emptyRow < initialState.size()-1) {
+			vector<vector<int>> consideredState = currentNode.state;
+			consideredState[emptyRow][emptyCol] = consideredState[emptyRow+1][emptyCol];
+			consideredState[emptyRow+1][emptyCol] = 0;
+
+			// Enqueue the node only if it has not been seen before
+			string newKey = puzzleToString(consideredState);
+			if (repeatedStates.find(newKey) == repeatedStates.end()) {
+				BoardStateNode newNode;
+				newNode.state = consideredState;
+				newNode.depth = currentDepth;
+				nodesToExpand.push(newNode);
+				repeatedStates.insert({newKey, true});
+			}	
+		}
+
+		if (nodesToExpand.size() > maxQueueSize)
+			maxQueueSize = nodesToExpand.size();
+	}
+
+	// If this code is reached, there is no solution to the problem space given
+	cout << "No solution exists!\n";
+	cout << "Number of nodes expanded: " << numberNodesExpanded << endl;
+	cout << "Max queue size: " << maxQueueSize;
 }
